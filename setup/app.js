872cbd98c5b49a72927d641373a31b016e0579f9
Broc Hammett <bxrayhamm@gmail.com>
2021-09-20 21:55:35 -0500

    fixed navbar and links

--- setup/app.js
+++ setup/app.js
@@ -17,15 +17,34 @@ navToggle.addEventListener('click', function() {
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const linksHeight = links.getBoundingClientRect().height;
     
-    if(containerHeight === 0){
+    if(containerHeight === 0) {
         linksContainer.style.height = `${linksHeight}px`;
     }
     else {
         linksContainer.style.height = 0; 
     }
-})
+});
+
+const navbar = document.getElementById('nav'); 
+const topLink = document.querySelector('.top-link');
 
 // ********** fixed navbar ************
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset; 
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav'); 
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link'); 
+    }
+});
 
 // ********** smooth scroll ************
 // select links
